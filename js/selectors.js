7dd567b70fafa0bffbb68bd3ce9c485d54d2ad7c
John Kalinauskas <jkalinauskas@live.com>
2018-03-27 19:48:38 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,7 +1,15 @@
 
   
-}
 function ninjaBabySelector() {
  return $('#baby-ninja')
   
 }
+
+
+//_______________________________________
+function paragraphSelector() {
+  return $('p');
+  }
+
+function lastImageSelector()  {
+  return $("https://s3.amazonaws.com/after-school-assets/confused.gif");
